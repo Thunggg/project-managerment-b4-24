e159e674aa69e5a703f275a0831c12b46e007573
Thunggg <thuannguyen20041028@gmail.com>
2024-08-22 11:52:20 +0700

    6.4. Làm trang danh sách sản phẩm bên Client

--- controller/client/product.controller.js
+++ controller/client/product.controller.js
@@ -1,6 +1,18 @@
+const Product = require("../../models/product.model");
+
 // [GET] /products/
-module.exports.index = (req, res) => {
-    res.render('client/pages/products/index.pug', {
-        pageTitle: "danh sách sản phẩm"
-    })
+module.exports.index = async (req, res) => {
+  const products = await Product.find({
+    status: "active",
+    deleted: false
+  });
+
+  for(const product of products){
+    product.priceNew = ((1 - product.discountPercentage / 100) * product.price).toFixed(0);
+  }
+
+  res.render("client/pages/products/index", {
+    pageTitle: "Danh sách sản phẩm",
+    products: products
+  });
 }
