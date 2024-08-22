04d33958e127d59913b96841bbac11ce04faa71b
Thunggg <thuannguyen20041028@gmail.com>
2024-08-22 15:59:56 +0700

    3. Tính năng lọc theo trạng thái - Frontend

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,1 +1,28 @@
-console.log("OK");
+// ---------------------------------------------button status---------------------------------------------
+
+const listButtonStatus = document.querySelectorAll("[button-status]");
+if(listButtonStatus.length > 0){
+    let url = new URL(window.location.href);
+
+    listButtonStatus.forEach(button => {
+        button.addEventListener("click", () => {
+            const status = button.getAttribute("button-status");
+            
+            if(status){
+                url.searchParams.set("status", status);
+            }
+            else{
+                url.searchParams.delete("status");
+            }
+
+            window.location.href = url.href;
+        });
+    });
+
+    // Thêm class active khi người dùng chọn
+    const statusCurrent = url.searchParams.get("status") || "";
+    const buttonCurrent = document.querySelector(`[button-status="${statusCurrent}"]`);
+    buttonCurrent.classList.add("active");
+}
+
+// -------------------------------------------------------------------------------------------------------
