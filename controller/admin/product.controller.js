276d99e7e4aa92de1299f2c1d1e0d6b1b121b808
Thunggg <thuannguyen20041028@gmail.com>
2024-08-22 15:05:01 +0700

    2. Lấy data sản phẩm in ra giao diện

--- controller/admin/product.controller.js
+++ controller/admin/product.controller.js
@@ -1,6 +1,16 @@
+const Product = require("../../models/product.model");
+
 // [GET] /admin/products
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+
+    const products = await Product.find({
+        status: "active",
+        deleted: false
+    });
+
+
     res.render('admin/pages/products/index',{
-        pageTitle: "trang quản lý sản phẩm"
+        pageTitle: "trang quản lý sản phẩm",
+        products: products
     });
 }
