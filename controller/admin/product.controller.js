e819b9c2e8c6697c9938d0dd8a0f2702fcffb204
Thunggg <thuannguyen20041028@gmail.com>
2024-08-24 00:29:28 +0700

    4.2. Xóa mềm

--- controller/admin/product.controller.js
+++ controller/admin/product.controller.js
@@ -99,8 +99,10 @@ module.exports.deleteItem = async (req, res) => {
     const id = req.params.id;
 
 
-    await Product.deleteOne({
+    await Product.updateOne({
     _id: id
+  }, {
+    deleted: true
   });
 
     res.json({
