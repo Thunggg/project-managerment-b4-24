8d68a457f569f128e8430b1c65d99180b98e53c9
Thunggg <thuannguyen20041028@gmail.com>
2024-08-23 11:35:13 +0700

    4. Tính năng tìm kiếm - Frontend fix tìm kiêm theo nhiều tiêu chí

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -26,3 +26,28 @@ if(listButtonStatus.length > 0){
 }
 
 // -------------------------------------------------------------------------------------------------------
+
+// ---------------------------------------------[form search]---------------------------------------------
+// fix lỗi khi bấm vào các nút button status rồi tìm kiếm thì bị reset lại thanh tìm kiếm
+const formSearch = document.querySelector("[form-search]");
+
+if(formSearch){
+    formSearch.addEventListener("submit", (event) => {
+        event.preventDefault();
+        
+        let url = new URL(window.location.href);
+
+        const keyword = event.target.elements.keyword.value;
+
+        if(keyword){
+            url.searchParams.set("keyword", keyword);
+        }
+        else{
+            url.searchParams.delete("keyword");
+        }
+
+        window.location.href = url;
+    });
+}
+// -------------------------------------------------------------------------------------------------------
+
