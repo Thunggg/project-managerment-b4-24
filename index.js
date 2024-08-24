edf2b4484d8f993dcd1f4652f4e10af0b9533880
Thunggg <thuannguyen20041028@gmail.com>
2024-08-24 09:01:08 +0700

    4. Tính năng tạo mới sản phẩm - Cơ bản

--- index.js
+++ index.js
@@ -18,11 +18,6 @@ const database = require("./config/database");
 database.connect();
 // ---------------------------------------------
 
-const routeClient = require("./routes/client/index.route"); //route cuả client
-routeClient.index(app);
-
-const routeAdmin = require("./routes/admin/index.route"); //route cuả admin
-routeAdmin.index(app);
 
 // --------------[kết nối database]-----------------
 const systemConfig = require("./config/system");
@@ -32,13 +27,22 @@ app.locals.prefixAdmin = systemConfig.prefixAdmin
 const port = process.env.PORT;
 
 app.use(express.static('public')); // nhúng file tĩnh
+app.use(bodyParser.urlencoded({ extended: false })); // dùng để lấy dữ liệu từ form thông qua phần body mà ông Fe gửi lên
 app.use(bodyParser.json());// dùng để chuyển đổi dữ liệu (được gửi từ FE qua phần body) sang js
-
 
 // --------------[khởi tạo pug]-----------------
 app.set("views", "./views");
 app.set("view engine", "pug");
 // ---------------------------------------------
+
+
+
+const routeClient = require("./routes/client/index.route"); //route cuả client
+routeClient.index(app);
+
+const routeAdmin = require("./routes/admin/index.route"); //route cuả admin
+routeAdmin.index(app);
+
 
 app.listen(port, () => {
     console.log('dang chạy cổng 3000')
