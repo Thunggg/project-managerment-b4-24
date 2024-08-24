1d775bde0e59a3be8b03a9e72f0e406d93741c07
Thunggg <thuannguyen20041028@gmail.com>
2024-08-24 11:52:17 +0700

    4. Tính năng tạo mới sản phẩm - Upload ảnh

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const multer  = require('multer'); // dùng để upload ảnh
+
+const storageMulterHelper = require("../../helpers/storageMulter.helper");
 
 const controller = require("../../controller/admin/product.controller");
+
+const upload = multer({ storage: storageMulterHelper.storage });
 
 router.get('/', controller.index);
 
@@ -15,6 +20,10 @@ router.patch("/change-position/:id", controller.changePosition);
 
 router.get("/create", controller.create);
 
-router.post("/create", controller.createPost);
+router.post(
+    "/create",
+    upload.single('thumbnail'),
+    controller.createPost
+);
 
 module.exports = router;
