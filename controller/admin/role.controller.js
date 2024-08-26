70a1af7fed80089e242cb98e91e04ebf3abd56c9
Thunggg <thuannguyen20041028@gmail.com>
2024-08-27 02:01:05 +0700

    3.3. Xây dựng phần Thêm mới nhóm quyền

--- controller/admin/role.controller.js
+++ controller/admin/role.controller.js
@@ -1,6 +1,7 @@
 const Role = require("../../models/role.model");
+const systemConfig = require("../../config/system");
 
-// [GET] /admin/products/create
+// [GET] /admin/roles
 module.exports.index = async (req, res) => {
 
     const records = await Role.find({
@@ -8,7 +9,23 @@ module.exports.index = async (req, res) => {
     });
 
     res.render('admin/pages/roles/index',{
-        pageTitle: "Thêm mới sản phẩm",
+        pageTitle: "Thêm mới nhóm quyền",
         records: records
     });
 }
+
+// [GET] /admin/roles/create
+module.exports.create = async (req, res) => {
+    res.render("admin/pages/roles/create", {
+      pageTitle: "Tạo mới nhóm quyền",
+    });
+};
+
+// [GET] /admin/roles/create
+module.exports.createPost = async (req, res) => {
+
+    const records = new Role(req.body);
+    records.save();
+
+    res.redirect(`/${systemConfig.prefixAdmin}/roles`);
+};
