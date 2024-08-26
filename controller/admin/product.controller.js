9e9da688fbd46d2837f94f32296ae03590d8c4d5
Thunggg <thuannguyen20041028@gmail.com>
2024-08-27 00:26:52 +0700

    2.1. Phần thêm mới sản phẩm

--- controller/admin/product.controller.js
+++ controller/admin/product.controller.js
@@ -3,6 +3,9 @@ const systemConfig = require("../../config/system");
 
 const paginationHelper = require("../../helpers/pagination.helper");
 
+const ProductCategory = require("../../models/product-category.model");
+const createTreeHelper = require("../../helpers/createTree.helper");
+
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
     const find = {
@@ -166,9 +169,15 @@ module.exports.changePosition = async (req, res) => {
 }
 
 // [GET] /admin/products/create
-module.exports.create = (req, res) => {
+module.exports.create = async (req, res) => {
+    const categories = await ProductCategory.find({
+        deleted: false
+    });
+    const newCategories = createTreeHelper(categories);
+
     res.render('admin/pages/products/create',{
-        pageTitle: "Thêm mới sản phẩm"
+        pageTitle: "Thêm mới sản phẩm",
+        categories: newCategories
     });
 }
 
