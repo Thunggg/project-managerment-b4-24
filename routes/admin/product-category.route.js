17b82f663fc878b74affa4b468f980c9d80d8232
Thunggg <thuannguyen20041028@gmail.com>
2024-08-27 00:03:23 +0700

    1. Tính năng chỉnh sửa danh mục sản phẩm

--- routes/admin/product-category.route.js
+++ routes/admin/product-category.route.js
@@ -18,5 +18,15 @@ router.post(
     controller.createPost
 );
 
+router.get("/edit/:id", controller.edit);
+
+router.patch(
+    "/edit/:id", 
+    upload.single('thumbnail'),
+    uploadCloud.uploadSingle,
+    validate.createPost,
+    controller.editPatch
+  );
+
 
 module.exports = router;
