8c72b24f2b2da5d05131b01e73bf9e049bf74346
Thunggg <thuannguyen20041028@gmail.com>
2024-08-27 12:59:25 +0700

    1.4. Sửa trang danh sách tài khoản

--- controller/admin/account.controller.js
+++ controller/admin/account.controller.js
@@ -5,12 +5,27 @@ const Account = require("../../models/account.model");
 const systemConfig = require("../../config/system");
 const generateHelper = require("../../helpers/generate.helper");
 
-// [GET] /admin/account
-module.exports.index = (req, res) => {
-    res.render('admin/pages/accounts/index',{
-        pageTitle: "Tài khoản admin"
+// [GET] /admin/accounts
+module.exports.index = async (req, res) => {
+    const records = await Account.find({
+      deleted: false
     });
-}
+  
+    for (const record of records) {
+      const role = await Role.findOne({
+        _id: record.role_id,
+        deleted: false
+      });
+  
+      record.roleTitle = role.title;
+    }
+    
+
+    res.render("admin/pages/accounts/index", {
+      pageTitle: "Tài khoản admin",
+      records: records
+    });
+  }
 
 // [GET] /admin/account/create
 module.exports.create = async (req, res) => {
