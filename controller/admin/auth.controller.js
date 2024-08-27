304fbcfdbe44b04f1098633d81d3eb5bf91160f1
Thunggg <thuannguyen20041028@gmail.com>
2024-08-27 14:30:25 +0700

    2. Làm tính năng đăng nhập

---
+++ controller/admin/auth.controller.js
@@ -0,0 +1,46 @@
+const Account = require("../../models/account.model");
+const md5 = require("md5");
+const systemConfig = require("../../config/system");
+
+// [GET] /admin/auth/login
+module.exports.login = async (req, res) => {
+    res.render("admin/pages/auth/login", {
+      pageTitle: "Đăng nhập"
+    });
+}
+
+// [POST] /admin/auth/login
+module.exports.loginPost = async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const account = await Account.findOne({
+        email: email,
+        deleted: false
+    });
+
+    if (!account){
+        req.flash("error", "Email Không tồn tại!");
+        res.redirect("back");
+        return;
+    }
+
+    if(md5(password) != account.password) {
+        req.flash("error", "Sai mật khẩu!");
+        res.redirect("back");
+        return;
+    }
+    
+    if(account.status != "active") {
+        req.flash("error", "Tài khoản đang bị khóa!");
+        res.redirect("back");
+        return;
+    }
+
+    req.flash("success", "đăng nhập thành công!");
+    
+    res.cookie("token", account.token);
+    res.redirect(`/${systemConfig.prefixAdmin}/dashboard`);
+
+    // res.send("OK");
+}
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -3,6 +3,7 @@ const productsRoute = require("./product.route");
 const productsCategoryRoute = require("./product-category.route");
 const rolesRoute = require("./role.route");
 const accountsRoute = require("./account.route");
+const authRoute = require("./auth.route");
 
 const systemConfig = require("../../config/system");
 
@@ -18,4 +19,6 @@ module.exports.index = (app) => {
     app.use(`${path}/roles`, rolesRoute);
     
     app.use(`${path}/accounts`, accountsRoute);
+
+    app.use(`${path}/auth`, authRoute);
 }
