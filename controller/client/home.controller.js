328ccb107675a7146caa091a2d77ef96d38db4a5
Thunggg <thuannguyen20041028@gmail.com>
2024-08-28 17:21:03 +0700

    4.1 và 4.2

--- controller/client/home.controller.js
+++ controller/client/home.controller.js
@@ -1,6 +1,39 @@
+const Product = require("../../models/product.model");
+
 // [GET] /
 module.exports.index = async (req, res) => {
+
+    // Sản phẩm nổi bật
+    const productsFeatured = await Product.find({
+        featured: "1",
+        deleted: false,
+        status: "active"
+    })
+    .sort({ position: "desc" })
+    .limit(6)
+    .select("-description");
+
+    for(item of productsFeatured){
+        item.priceNew = ((1 - item.discountPercentage / 100) * item.price).toFixed(0);
+    }
+
+    //Sản phẩm mới nhất
+    const productsNew = await Product
+    .find({
+      status: "active",
+      deleted: false
+    })
+    .sort({ position: "desc" })
+    .limit(6)
+    .select("-description");
+
+    for (const item of productsNew) {
+        item.priceNew = ((1 - item.discountPercentage/100) * item.price).toFixed(0);
+    }
+
     res.render('client/pages/home/index.pug',{
-        pageTitle: "trang chủ"
+        pageTitle: "trang chủ",
+        productsFeatured: productsFeatured,
+        productsNew: productsNew
     });
 }
