f550fa4f55cbb1a3aad4666abf9eec4977e2b687
Thunggg <thuannguyen20041028@gmail.com>
2024-08-28 13:39:34 +0700

    1. Thêm authorization

--- controller/admin/product.controller.js
+++ controller/admin/product.controller.js
@@ -76,96 +76,123 @@ else{
 // [PATCH] /admin/products/change-status/:statusChange/:id
 module.exports.changeStatus = async (req, res) => {
     
-    try {
-        const {id, statusChange} = req.params;
-
-        await Product.updateOne({
-            _id: id
-        }, {
-            status: statusChange
-        });
-
-        req.flash('success', 'Cập nhật trạng thái thành công!');
-        
-        res.json({
-            code: 200
-    });
-    } catch (error) {
-        res.redirect(`/${systemConfig.prefixAdmin}/products`);
+    if(res.locals.role.permissions.includes("products_edit")){
+        try {
+            const {id, statusChange} = req.params;
+    
+            await Product.updateOne({
+                _id: id
+            }, {
+                status: statusChange
+            });
+    
+            req.flash('success', 'Cập nhật trạng thái thành công!');
+            
+            res.json({
+                code: 200
+            });
+        } catch (error) {
+            res.redirect(`/${systemConfig.prefixAdmin}/products`);
+        }
+    } else{
+        res.send(`403`);
     }
+
+    
 }
 
 // [PATCH] /admin/products/change-multi
 module.exports.changeMulti = async (req, res) => {
 
-    const {ids, status} = req.body;
+    if(res.locals.role.permissions.includes("products_edit")){
+        const {ids, status} = req.body;
 
-    switch (status){
-        case "active":
-        case "inactive":
-            await Product.updateMany({
-                _id: ids
-            },{
-                status: status
-            });
-            break;
-        case "delete":
-            await Product.updateMany({
-                _id: ids
-            },{
-                deleted: true
-            });
-            break;
-        default:
-            break;
+        switch (status){
+            case "active":
+            case "inactive":
+                await Product.updateMany({
+                    _id: ids
+                },{
+                    status: status
+                });
+                break;
+            case "delete":
+                await Product.updateMany({
+                    _id: ids
+                },{
+                    deleted: true
+                });
+                break;
+            default:
+                break;
+        }
+
+        req.flash('success', 'Cập nhật sản phẩm thành công!');
+
+
+        res.json({
+            code: 200
+        });
+
+
+    } else{
+        res.send(`403`);
     }
 
-    res.json({
-        code: 200
-    });
+    
 }
 
 // [PATCH] /admin/products/delete/:id
 module.exports.deleteItem = async (req, res) => {
-
-    try {
-        const id = req.params.id;
-
-        await Product.updateOne({
-            _id: id
-        }, {
-            deleted: true
-        });
-
-        req.flash('success', 'Xóa sản phẩm thành công!');
-
-        res.json({
-            code: 200
-        });
-    } catch (error) {
-        res.redirect(`/${systemConfig.prefixAdmin}/products`);
+    if(res.locals.role.permissions.includes("products_delete")){
+        try {
+            const id = req.params.id;
+    
+            await Product.updateOne({
+                _id: id
+            }, {
+                deleted: true
+            });
+    
+            req.flash('success', 'Xóa sản phẩm thành công!');
+    
+            res.json({
+                code: 200
+            });
+        } catch (error) {
+            res.redirect(`/${systemConfig.prefixAdmin}/products`);
+        }
+    } else{
+        res.send(`403`);
     }
+    
 }
 
 // [PATCH] /admin/products/change-position/:id
 module.exports.changePosition = async (req, res) => {
+    if(res.locals.role.permissions.includes("products_edit")){
 
-    try {
-        const id = req.params.id;
-        const position = req.body.position;
+        try {
+            const id = req.params.id;
+            const position = req.body.position;
+    
+            await Product.updateOne({
+                _id: id
+            },{
+                position: req.body.position
+            });
+    
+            res.json({
+                code: 200
+            });
+        } catch (error) {
+            res.redirect(`/${systemConfig.prefixAdmin}/products`);
+        }
 
-        await Product.updateOne({
-            _id: id
-        },{
-            position: req.body.position
-        });
-
-        res.json({
-            code: 200
-        });
-    } catch (error) {
-        res.redirect(`/${systemConfig.prefixAdmin}/products`);
+    } else{
+        res.send(`403`);
     }
+    
 }
 
 // [GET] /admin/products/create
@@ -183,28 +210,34 @@ module.exports.create = async (req, res) => {
 
 // [POST] /admin/products/create
 module.exports.createPost = async (req, res) => {
+    if(res.locals.role.permissions.includes("products_create")){
 
-    req.body.price = parseInt(req.body.price);
-    req.body.discountPercentage = parseInt(req.body.discountPercentage);
-    req.body.stock = parseInt(req.body.stock);
+        req.body.price = parseInt(req.body.price);
+        req.body.discountPercentage = parseInt(req.body.discountPercentage);
+        req.body.stock = parseInt(req.body.stock);
 
-    
-    // if(req.file.filename){ do đã up lên cloudiary nên ko cần nữa
-    //     req.body.thumbnail = `/uploads/${req.file.filename}`;
-    // }
-    
-    if(req.body.position){
-        req.body.position = parseInt(req.body.position);
+        
+        // if(req.file.filename){ do đã up lên cloudiary nên ko cần nữa
+        //     req.body.thumbnail = `/uploads/${req.file.filename}`;
+        // }
+        
+        if(req.body.position){
+            req.body.position = parseInt(req.body.position);
+        } else{
+            let countProduct = await Product.countDocuments({});
+            req.body.position = countProduct + 1;
+        }
+
+        const newProduct = new Product(req.body);
+        await newProduct.save();
+
+
+        res.redirect(`/${systemConfig.prefixAdmin}/products`);
+
     } else{
-        let countProduct = await Product.countDocuments({});
-        req.body.position = countProduct + 1;
+        res.send(`403`);
     }
-
-    const newProduct = new Product(req.body);
-    await newProduct.save();
-
-
-    res.redirect(`/${systemConfig.prefixAdmin}/products`);
+    
 
 }
 
@@ -242,38 +275,44 @@ module.exports.edit = async (req, res) => {
 
 // [PATCH] /admin/products/edit/:id
 module.exports.editPatch = async (req, res) => {
+    if(res.locals.role.permissions.includes("products_edit")){
 
-    try {
-        const id = req.params.id;
-
-        // if(req.file && req.file.filename){ do đã up lên cloudiary nên ko cần nữa
-        //     req.body.thumbnail = `/uploads/${req.file.filename}`;
-        // }
-
-        req.body.price = parseInt(req.body.price);
-        req.body.discountPercentage = parseInt(req.body.discountPercentage);
-        req.body.stock = parseInt(req.body.stock);
-
-        if(req.body.position){
-            req.body.position = parseInt(req.body.position);
-        } else{
-            let countProduct = await Product.countDocuments({});
-            req.body.position = countProduct + 1;
+        try {
+            const id = req.params.id;
+    
+            // if(req.file && req.file.filename){ do đã up lên cloudiary nên ko cần nữa
+            //     req.body.thumbnail = `/uploads/${req.file.filename}`;
+            // }
+    
+            req.body.price = parseInt(req.body.price);
+            req.body.discountPercentage = parseInt(req.body.discountPercentage);
+            req.body.stock = parseInt(req.body.stock);
+    
+            if(req.body.position){
+                req.body.position = parseInt(req.body.position);
+            } else{
+                let countProduct = await Product.countDocuments({});
+                req.body.position = countProduct + 1;
+            }
+    
+            await Product.updateOne({
+                _id: id,
+                deleted: false
+            }, req.body);
+    
+            req.flash("success", "Cập nhật sản phẩm thành công!");
+    
+    
+        } catch (error) {
+            req.flash("error", "Cập nhật sản phẩm thất bại");
         }
+    
+        res.redirect("back");
 
-        await Product.updateOne({
-            _id: id,
-            deleted: false
-        }, req.body);
-
-        req.flash("success", "Cập nhật sản phẩm thành công!");
-
-
-    } catch (error) {
-        req.flash("error", "Cập nhật sản phẩm thất bại");
+    } else{
+        res.send(`403`);
     }
-
-    res.redirect("back");
+    
 }
 
 // [GET] /admin/products/detail/:id
