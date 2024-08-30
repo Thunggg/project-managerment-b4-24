aa501516b932a7a781d1a7cb7014f4a99dca5b97
Thunggg <thuannguyen20041028@gmail.com>
2024-08-30 11:48:34 +0700

    2.2. Đăng ký tài khoản (Lấy dữ liệu gửi lên backend + format lại dữ liệu)

---
+++ controller/client/user.controller.js
@@ -0,0 +1,26 @@
+const md5 = require("md5");
+const generateHelper = require("../../helpers/generate.helper"); 
+
+// [GET] /user/register
+module.exports.register = async (req, res) => {
+    res.render("client/pages/user/register", {
+      pageTitle: "Đăng ký tài khoản",
+    });
+};
+
+// [POST] /user/register
+module.exports.registerPost = async (req, res) => {
+
+    const userData = {
+        fullName: req.body.fullName,
+        email: req.body.email,
+        password:  md5(req.body.password),
+        tokenUser: generateHelper.generateRandomString(30)
+    }
+
+    res.send(userData);
+
+    // res.render("client/pages/user/register", {
+    //   pageTitle: "Đăng ký tài khoản",
+    // });
+};
