0063fb9e19268b2544a739de3967386b1a6b82e9
Thunggg <thuannguyen20041028@gmail.com>
2024-08-30 07:47:30 +0700

    2.5. Cập nhật số lượng sản phẩm trong giỏ hàng

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,16 @@
-console.log("ok");
+// Cập nhật số lượng sản phẩm trong giỏ hàng
+const listInputQuantity = document.querySelectorAll("table[cart] input[name='quantity']");
+if(listInputQuantity.length > 0){
+    listInputQuantity.forEach(button => {
+        button.addEventListener("change", () => {
+            const productId = button.getAttribute("product-id");
+            const quantity = button.value;
+            
+            if(productId && quantity){
+                window.location.href = `/cart/update/${productId}/${quantity}`;
+            }
+
+        });
+    });
+}
+// Hết Cập nhật số lượng sản phẩm trong giỏ hàng
