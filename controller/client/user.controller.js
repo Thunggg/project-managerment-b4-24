3dc444f0859a8b9e992adbbd82b17581d525baee
Thunggg <thuannguyen20041028@gmail.com>
2024-08-30 11:59:43 +0700

    2.2. Đăng ký tài khoản (Check email tồn tại, lưu dữ liệu, trả về token)

--- controller/client/user.controller.js
+++ controller/client/user.controller.js
@@ -1,3 +1,5 @@
+const User = require("../../models/user.model");
+
 const md5 = require("md5");
 const generateHelper = require("../../helpers/generate.helper"); 
 
@@ -11,16 +13,28 @@ module.exports.register = async (req, res) => {
 // [POST] /user/register
 module.exports.registerPost = async (req, res) => {
 
+    const exitUser = await User.findOne({
+        email: req.body.email
+    });
+
+    if(exitUser){
+        req.flash("error", "Email đã tồn tại");
+        res.redirect("back");
+        return;
+    }
+
     const userData = {
         fullName: req.body.fullName,
         email: req.body.email,
         password:  md5(req.body.password),
         tokenUser: generateHelper.generateRandomString(30)
-    }
+    };
 
-    res.send(userData);
+    const user = new User(userData);
+    await user.save();
 
-    // res.render("client/pages/user/register", {
-    //   pageTitle: "Đăng ký tài khoản",
-    // });
+    res.cookie("tokenUser", user.tokenUser);
+
+    req.flash("success", "Đăng ký tài khoản thành công!");
+    res.redirect("/");
 };
