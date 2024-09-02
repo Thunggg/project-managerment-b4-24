d191605e98fa7fe3f9304987142f3d250075ee3d
Thunggg <thuannguyen20041028@gmail.com>
2024-09-02 15:23:39 +0700

    Tối ưu file socket

--- controller/client/chat.controller.js
+++ controller/client/chat.controller.js
@@ -1,76 +1,27 @@
 const Chat = require("../../models/chat.model");
 const User = require("../../models/user.model");
-const streamUpload = require("../../helpers/streamUpload.helper");
+
+const chatSocket = require("../../sockets/client/chat.socket");
 
 // [GET] /chat/
 module.exports.index = async (req, res) => {
-    const userId = res.locals.user.id;  
-    const fullName = res.locals.user.fullName;  
+  
+  // SocketIO
+  chatSocket(req, res);
+  // End SocketIO
 
-    //socket io
+  const chats = await Chat.find({});
 
-    _io.once("connection", (socket) => {
-        
-        // CLIENT_SEND_MESSAGE
-        socket.on("CLIENT_SEND_MESSAGE", async (data) =>{
-            const chatData ={
-                userId: userId,
-                content: data.content
-            }
-            const linkImages = [];
-
-            for (const image of data.images) {
-                const result = await streamUpload(image);
-                linkImages.push(result.url);
-            }
-
-            chatData.images = linkImages;
-            
-            // Lưu data vào database
-            const chat = new Chat(chatData);
-            await chat.save();
-
-            // Trả tin nhắn realtime về cho mọi người (Làm sau)
-            _io.emit("SERVER_RETURN_MESSAGE", {
-                userId: userId,
-                fullName: fullName,
-                content: data.content,
-                images: linkImages
-            });
-
-        });
-        // END CLIENT_SEND_MESSAGE
-
-        // CLIENT_SEND_TYPING
-        socket.on("CLIENT_SEND_TYPING", (type) =>{
-            socket.broadcast.emit("SERVER_RETURN_TYPING", {
-                userId: userId,
-                fullName: fullName,
-                type: type
-            });
-        });
-        // END CLIENT_SEND_TYPING
-
-
+  for (const chat of chats) {
+    const infoUser = await User.findOne({
+      _id: chat.userId
     });
 
+    chat.fullName = infoUser.fullName;
+  }
 
-
-    //END socket io
-
-    const chats = await Chat.find({});
-
-    for(const chat of chats){
-        const infoUser = await User.findOne({
-            _id: chat.userId
-        });
-
-        chat.fullName = infoUser.fullName;
-    }
-
-    res.render("client/pages/chat/index", {
-        pageTitle: "Chat",
-        chats: chats
-    });
-    
+  res.render("client/pages/chat/index", {
+    pageTitle: "Chat",
+    chats: chats
+  });
 };
