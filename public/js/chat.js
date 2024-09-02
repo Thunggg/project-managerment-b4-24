c3ea067e87e20a11ae38d132387b6782e5276a8e
Thunggg <thuannguyen20041028@gmail.com>
2024-09-02 16:42:48 +0700

    1.4. Hiển thị ảnh full màn hình

--- public/js/chat.js
+++ public/js/chat.js
@@ -119,6 +119,7 @@ socket.on("SERVER_RETURN_MESSAGE", (data) => {
     body.insertBefore(div, elementListTyping);
   
     body.scrollTop = body.scrollHeight;
+    new Viewer(div);
   });
   // End SERVER_RETURN_MESSAGE
 
@@ -152,3 +153,9 @@ if(buttonIcon) {
   });
 }
 // End Show Popup Icon
+
+// Preview Image
+if(bodyChat) {
+  new Viewer(bodyChat);
+}
+// End Preview Image
